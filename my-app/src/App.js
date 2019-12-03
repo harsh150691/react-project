5defc5f4e1697c60f970598b5b90ecc21debdaf2
MacShine <harsh@MacShines-MacBook-Pro.local>
2019-12-03 21:09:47 +0530

    to see changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning for the project
+          Learning data
             
           }
         </a>
